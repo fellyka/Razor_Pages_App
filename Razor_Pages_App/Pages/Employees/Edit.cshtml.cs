abcf01351cc0ebb165f8c6b00be2b01e7a5d37e4
Felly KANYIKI NGOY <fellyka@gmail.com>
2023-05-11 08:44:42 +0200

    Editing Data

---
+++ Razor_Pages_App/Pages/Employees/Edit.cshtml.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using Razor_Pages_App.Models;
+using Razor_Pages_App.Services;
+
+namespace Razor_Pages_App.Pages.Employees
+{
+    public class EditModel : PageModel
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EditModel(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        public Employee Employee {get;set;}
+
+        public IActionResult OnGet(int id)//Model binding will automatically bind the id with the route parameter
+        {
+          Employee = _employeeRepository.GetEmployee(id);
+
+          if (Employee == null)
+            {
+                return RedirectToPage("/NotFound");
+            }
+
+            return Page();
+        }
+   
+    }
+}
